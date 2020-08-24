04275ae980cb1a5a770207f762e5f169201b0fa5
Jesse Schulman <jesse@caleos.io>
2020-08-24 09:34:54 -0700

    Adding check for valid Telos account name

--- contracts/tradefortlos/src/tradefortlos.cpp
+++ contracts/tradefortlos/src/tradefortlos.cpp
@@ -4,8 +4,8 @@ tradefortlos::tradefortlos(name self, name code, datastream<const char *> ds) : 
 
 tradefortlos::~tradefortlos() {}
 
-ACTION tradefortlos::noop() {
-
+ACTION tradefortlos::noop()
+{
 }
 
 void tradefortlos::ontransfer(name from, name to, asset quantity, string memo)
@@ -14,17 +14,22 @@ void tradefortlos::ontransfer(name from, name to, asset quantity, string memo)
 
     if (rec == CORE_SYM_ACCOUNT && from != get_self() && quantity.symbol == CORE_SYM)
     {
-        action(permission_level{ get_self(), name("active") }, CORE_SYM_ACCOUNT, name("transfer"),
-            make_tuple(get_self(),
-                TLOS_BUYER,
-                quantity,
-                std::string("TLOS auto-buy")))
+        check(!memo.empty(), "Telos account name is empty");
+
+        // Leverage checks in name constructor for validation
+        name telos_acct = name(memo);
+
+        action(permission_level{get_self(), name("active")}, CORE_SYM_ACCOUNT, name("transfer"),
+               make_tuple(get_self(),
+                          TLOS_BUYER,
+                          quantity,
+                          std::string("TLOS auto-buy")))
             .send();
-        action(permission_level{ TLOS_BUYER, name("active") }, CORE_SYM_ACCOUNT, name("transfer"),
-            make_tuple(TLOS_BUYER,
-                name("newdexpocket"),
-                quantity,
-                std::string("{\"type\" : \"buy-market\", \"symbol\" : \"eosio.token-tlos-eos\", \"price\" : \"0.000000\", \"channel\" : \"web\", \"receiver\" : \"" + memo + "\"}")))
+        action(permission_level{TLOS_BUYER, name("active")}, CORE_SYM_ACCOUNT, name("transfer"),
+               make_tuple(TLOS_BUYER,
+                          name("newdexpocket"),
+                          quantity,
+                          std::string("{\"type\" : \"buy-market\", \"symbol\" : \"eosio.token-tlos-eos\", \"price\" : \"0.000000\", \"channel\" : \"web\", \"receiver\" : \"" + memo + "\"}")))
             .send();
     }
 }
